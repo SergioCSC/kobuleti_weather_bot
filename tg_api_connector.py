3a9a68c20acc60437387f6de5616c5365c28f276
Sergei Epifanov <pifych@gmail.com>
2023-01-18 18:06:13 +0400

    clear old comments

--- tg_api_connector.py
+++ tg_api_connector.py
@@ -1,38 +1,24 @@
 import json
 
 import sys
-from datetime import datetime
 import http
 import urllib
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 
 
+TELEGRAM_URL_PREFIX = 'https://api.telegram.org/bot'
+BOT_TOKEN = '5887622494:AAEvqObQZf6AE8F7oOYrPCiV11r_t8siWrw'
+
+
 def is_in_debug_mode():
     gettrace = getattr(sys, 'gettrace', None)
     return bool(gettrace and gettrace())
-
-# SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
-# EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
-
-
-# def validate(res):
-#     '''Return False to trigger the canary
-
-#     Currently this simply checks whether the EXPECTED string is present.
-#     However, you could modify this to perform any number of arbitrary
-#     checks on the contents of SITE.
-#     '''
-#     return EXPECTED in res
-
-TELEGRAM_URL_PREFIX = 'https://api.telegram.org/bot'
-BOT_TOKEN = '5887622494:AAEvqObQZf6AE8F7oOYrPCiV11r_t8siWrw'
 
 
 def get(url: str) -> http.client.HTTPResponse:
     req = Request(url, headers={'User-Agent': 'AWS Lambda'})
     return urlopen(req)
-
 
 
 def get_chat_list() -> set[int]:
@@ -64,16 +50,11 @@ def get_chat_list() -> set[int]:
     return result
     
 
-
 def lambda_handler(event, context):
-    # WEATHER_SITE = 'http://dataservice.accuweather.com/currentconditions/v1/166879?apikey=Gvz2sY3fAxES8GHGVZeaoGsfwxVJA3gE'
     WEATHER_SITE = 'https://api.openweathermap.org/data/2.5/weather?lat=41.8141&lon=41.7739&units=metric&lang=ru&appid=11c0d3dc6093f7442898ee49d2430d20'
     print('Checking {} at {}...'.format(WEATHER_SITE, event['time']))
     result = get(WEATHER_SITE)
-    # return event['time']
     d = json.load(result)
-    # celsius = d[0]['Temperature']['Metric']['Value']
-    # weather_text = d[0]['WeatherText']
     celsius = float(d['main']['temp'])  # - 273.15
     pressure_mm_hg = int(float(d['main']['pressure']) * 3 / 4)
     humidity = d['main']['humidity']
@@ -95,9 +76,9 @@ def lambda_handler(event, context):
         except HTTPError as e:
             print(f'HTTPError for url: {TELEGRAM_SITE}\n\nException: {e}')
     return
-    
 
-if __name__ == '__main__':
+
+def test_run():
     test_event = {
         "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
         "detail-type": "Scheduled Event",
@@ -111,4 +92,7 @@ if __name__ == '__main__':
         "detail": {}
     }
     
-    lambda_handler(test_event, None)
+    lambda_handler(test_event, None)    
+
+if __name__ == '__main__':
+    test_run()

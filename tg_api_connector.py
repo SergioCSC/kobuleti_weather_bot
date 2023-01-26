01dfc770015f2951cc66254885ddcdb76d0b8857
Sergei Epifanov <pifych@gmail.com>
2023-01-26 19:56:21 +0400

    send_message() can send message without image

--- tg_api_connector.py
+++ tg_api_connector.py
@@ -8,32 +8,31 @@ import urllib
 from urllib.error import HTTPError
 
 
-# def is_in_debug_mode():
-#     gettrace = getattr(sys, 'gettrace', None)
-#     return bool(gettrace and gettrace())
-
-
 def send_message(chat_set: set[int], message: str, image: io.BytesIO) -> None:
+    message = message.replace('!', '\!')
     message = urllib.parse.quote(message.encode('utf-8'))
     message = message.replace('-', '\-')
+    message = message.replace('.', '\.')
     
     for chat_id in chat_set:
-        TELEGRAM_SITE = (
+        telegram_request_url = (
             f'{cfg.TELEGRAM_URL_PREFIX}'
             f'{api_keys.TELEGRAM_BOT_TOKEN}'
-            f'/sendPhoto'
+            f'/{"sendPhoto" if image else "sendMessage"}'
             f'?disable_notification=true'
             f'&parse_mode=MarkdownV2'
             f'&chat_id={chat_id}'
-            f'&caption={message}'
+            f'&{"caption" if image else "text"}={message}'
         )
-        print(TELEGRAM_SITE)
-        image.seek(0)
+        if image:
+            image.seek(0)
+        print(telegram_request_url)
         try:
-            result = requests.post(TELEGRAM_SITE, files={'photo': image})
+            result = requests.post(telegram_request_url, 
+                                   files={'photo': image} if image else {})
             if result.status_code != 200:
-                print(f'{TELEGRAM_SITE = }\n{result.text = }')
+                print(f'{telegram_request_url = }\n{result.text = }')
         except HTTPError as e:
-            print(f'HTTPError for url: {TELEGRAM_SITE}\n\nException: {e}')
-    
-    image.seek(0)
+            print(f'HTTPError for url: {telegram_request_url}\n\nException: {e}')
+    if image:
+        image.seek(0)
